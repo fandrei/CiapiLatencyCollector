b38a66654d7c029c167996268dfb313d123f4bfc
fandrei <andrei_f_11@mail.ru>
2012-03-01 15:31:11 +0700

    bugfix

--- LatencyCollectorCore/AppSettings.cs
+++ LatencyCollectorCore/AppSettings.cs
@@ -38,14 +38,14 @@ namespace LatencyCollectorCore
 					return null;
 
 				var encrypted = Convert.FromBase64String(PasswordEncrypted);
-				var data = ProtectedData.Unprotect(encrypted, AdditionalEntropy, DataProtectionScope.CurrentUser);
+				var data = ProtectedData.Unprotect(encrypted, AdditionalEntropy, DataProtectionScope.LocalMachine);
 				var res = Encoding.UTF8.GetString(data);
 				return res;
 			}
 			set
 			{
 				var data = Encoding.UTF8.GetBytes(value);
-				var encrypted = ProtectedData.Protect(data, AdditionalEntropy, DataProtectionScope.CurrentUser);
+				var encrypted = ProtectedData.Protect(data, AdditionalEntropy, DataProtectionScope.LocalMachine);
 				PasswordEncrypted = Convert.ToBase64String(encrypted);
 			}
 		}
