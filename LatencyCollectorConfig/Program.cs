8f1609e20e754c5c785ca075243b3930ccad52ce
fandrei <andrei_f_11@mail.ru>
2012-05-03 16:33:16 +0800

    added capability to set user name and password from command line

--- LatencyCollectorConfig/Program.cs
+++ LatencyCollectorConfig/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
+using CiapiLatencyCollector;
+using LatencyCollectorCore;
+
 namespace LatencyCollectorConfig
 {
 	static class Program
@@ -11,11 +14,57 @@ namespace LatencyCollectorConfig
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main()
+		static void Main(string[] args)
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new ConfigForm());
+			try
+			{
+				if (args.Length != 0)
+				{
+					var argsDic = new Dictionary<string, string>();
+					foreach (var arg in args)
+					{
+						if (arg.StartsWith("-") || arg.StartsWith("/"))
+						{
+							var tmp = arg.Substring(1);
+							if (!tmp.Contains(":"))
+							{
+								argsDic.Add(tmp, "");
+							}
+							else
+							{
+								var parts = tmp.Split(':');
+								if (parts.Count() != 2)
+									throw new ArgumentException("Invalid parameter", arg);
+								argsDic.Add(parts[0], parts[1]);
+							}
+						}
+					}
+
+					string userName;
+					argsDic.TryGetValue("username", out userName);
+
+					string password;
+					argsDic.TryGetValue("password", out password);
+
+					if (!string.IsNullOrEmpty(userName))
+					{
+						AppSettings.Instance.UserName = userName;
+						AppSettings.Instance.Password = password;
+
+						AppSettings.Instance.Save();
+					}
+				}
+				else
+				{
+					Application.EnableVisualStyles();
+					Application.SetCompatibleTextRenderingDefault(false);
+					Application.Run(new ConfigForm());
+				}
+			}
+			catch (Exception exc)
+			{
+				MessageBox.Show(exc.ToString(), Const.AppName);
+			}
 		}
 	}
 }
