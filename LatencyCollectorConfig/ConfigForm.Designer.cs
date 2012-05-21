bf550e5884ae9881d5917c6ec8db08b7a1b3726a
fandrei <andrei_f_11@mail.ru>
2012-05-21 17:07:25 +0800

    bugfix

--- LatencyCollectorConfig/ConfigForm.Designer.cs
+++ LatencyCollectorConfig/ConfigForm.Designer.cs
@@ -146,6 +146,8 @@
 			// 
 			// MonitorsEdit
 			// 
+			this.MonitorsEdit.AcceptsReturn = true;
+			this.MonitorsEdit.AcceptsTab = true;
 			this.MonitorsEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
