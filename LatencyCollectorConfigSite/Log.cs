6daffc125d3da7b57c5315b7c766a55b6fa40ca5
fandrei <andrei_f_11@mail.ru>
2012-06-06 17:30:59 +0800

    simple site to handle centralized configs data

---
+++ LatencyCollectorConfigSite/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LatencyCollectorConfigSite
+{
+	public static class Log
+	{
+		public static void Report(object val)
+		{
+			var msg = val.ToString();
+			Trace.WriteLine(msg);
+		}
+	}
+}
---
+++ LatencyCollectorConfigSite/WebUtil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace LatencyCollectorConfigSite
+{
+	public class WebUtil
+	{
+		public static void CheckIpAddress()
+		{
+			var request = HttpContext.Current.Request;
+			if (!request.IsLocal)
+			{
+				var response = HttpContext.Current.Response;
+				response.Write("Access from this IP address is not allowed");
+				response.Status = "401 Unauthorized";
+				response.StatusCode = 401;
+				response.End();
+				throw new UnauthorizedAccessException();
+			}
+		}
+
+		public static string AppDataPath
+		{
+			get
+			{
+				var res = HostingEnvironment.MapPath("~/App_Data");
+				return res;
+			}
+		}
+	}
+}
