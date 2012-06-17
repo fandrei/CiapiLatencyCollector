9bd95febf6e3a664c0f85560b50fff873e5f82d2
fandrei <andrei_f_11@mail.ru>
2012-06-17 16:17:21 +0700

    refactoring and code cleanup

--- LatencyCollectorConfigSite/CreateUser.aspx.cs
+++ LatencyCollectorConfigSite/CreateUser.aspx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using AppMetrics.WebUtils;
+
 namespace LatencyCollectorConfigSite
 {
 	public partial class CreateUser : System.Web.UI.Page
