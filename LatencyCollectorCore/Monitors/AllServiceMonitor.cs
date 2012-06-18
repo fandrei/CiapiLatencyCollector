dceec8a39c9a550432f80c9ffc396058ac11024d
bitpusher <sky.sanders@gmail.com>
2012-06-18 14:41:10 -0700

    recording

--- LatencyCollectorCore/Monitors/AllServiceMonitor.cs
+++ LatencyCollectorCore/Monitors/AllServiceMonitor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,202 +10,226 @@ using CIAPI.Rpc;
 
 namespace LatencyCollectorCore.Monitors
 {
-	public class AllServiceMonitor : AuthenticatedMonitor
-	{
-		public AllServiceMonitor()
-		{
-			ServerUrl = "https://ciapi.cityindex.com/TradingApi";
-			StreamingServerUrl = "https://push.cityindex.com";
-			PeriodSeconds = 5;
-		}
+    public class AllServiceMonitor : AuthenticatedMonitor
+    {
+        public AllServiceMonitor()
+        {
+            ServerUrl = "https://ciapi.cityindex.com/TradingApi";
+            StreamingServerUrl = "https://push.cityindex.com";
+            PeriodSeconds = 5;
+        }
 
-		public string ServerUrl { get; set; }
-		public string StreamingServerUrl { get; set; }
+        public string ServerUrl { get; set; }
+        public string StreamingServerUrl { get; set; }
 
-		public bool AllowTrading { get; set; }
+        public bool AllowTrading { get; set; }
 
-		private Client _client = null;
-		internal Client ApiClient
-		{
-			get
-			{
-				if (_client == null)
-				{
-					_client = new Client(new Uri(ServerUrl), new Uri(StreamingServerUrl), "{API_KEY}");
-					_client.AppKey = "CiapiLatencyCollector." + GetType().Name;
-				}
-				return _client;
-			}
-			set { _client = value; }
-		}
+        private Client _client = null;
+        internal Client ApiClient
+        {
+            get
+            {
+                if (_client == null)
+                {
+                    _client = new Client(new Uri(ServerUrl), new Uri(StreamingServerUrl), "{API_KEY}");
+                    _client.AppKey = "CiapiLatencyCollector." + GetType().Name;
+                }
+                return _client;
+            }
+            set { _client = value; }
+        }
 
-		// GBP/USD markets
-		private const int MarketId = 400616150;
+        // GBP/USD markets
+        private const int MarketId = 400616150;
 
-		public override void Execute()
-		{
-			try
-			{
+        public override void Execute()
+        {
+            
+            var recorder = new Salient.ReliableHttpClient.Recorder(ApiClient);
+
+            try
+            {
+
+
+                recorder.Start();
+                {
+                    var measure = Tracker.StartMeasure();
+                    ApiClient.LogIn(UserName, Password);
+                    Tracker.EndMeasure(measure, "LogIn");
+                }
+
+                AccountInformationResponseDTO accountInfo;
+                {
+                    var measure = Tracker.StartMeasure();
+                    accountInfo = ApiClient.AccountInformation.GetClientAndTradingAccount();
+                    Tracker.EndMeasure(measure, "GetClientAndTradingAccount");
+                }
+
+                {
+                    var measure = Tracker.StartMeasure();
+                    var resp = ApiClient.SpreadMarkets.ListSpreadMarkets("", "",
+                        accountInfo.ClientAccountId, 100, false);
+                    Tracker.EndMeasure(measure, "ListSpreadMarkets");
+                }
+
+                {
+                    var measure = Tracker.StartMeasure();
+                    var resp = ApiClient.News.ListNewsHeadlinesWithSource("dj", "UK", 10);
+                    Tracker.EndMeasure(measure, "ListNewsHeadlinesWithSource");
+                }
+
+                {
+                    var measure = Tracker.StartMeasure();
+                    var resp = ApiClient.Market.GetMarketInformation(MarketId.ToString());
+                    Tracker.EndMeasure(measure, "GetMarketInformation");
+                }
+
+                {
+                    var measure = Tracker.StartMeasure();
+                    var resp = ApiClient.PriceHistory.GetPriceBars(MarketId.ToString(), "MINUTE", 1, "20");
+                    Tracker.EndMeasure(measure, "GetPriceBars");
+                }
+
+                if (AllowTrading)
+                {
+                    var price = GetPrice(ApiClient);
+
+                    var orderId = Trade(ApiClient, accountInfo, price, 1M, "buy", new int[0]);
+
+                    {
+                        var measure = Tracker.StartMeasure();
+                        ApiClient.TradesAndOrders.ListOpenPositions(accountInfo.SpreadBettingAccount.TradingAccountId);
+                        Tracker.EndMeasure(measure, "ListOpenPositions");
+                    }
+
+                    Trade(ApiClient, accountInfo, price, 1M, "sell", new[] { orderId });
+                }
+                else
+                {
+                    var measure = Tracker.StartMeasure();
+                    ApiClient.TradesAndOrders.ListOpenPositions(accountInfo.SpreadBettingAccount.TradingAccountId);
+                    Tracker.EndMeasure(measure, "ListOpenPositions");
+                }
+
+                {
+                    var measure = Tracker.StartMeasure();
+                    var tradeHistory = ApiClient.TradesAndOrders.ListTradeHistory(accountInfo.SpreadBettingAccount.TradingAccountId, 20);
+                    Tracker.EndMeasure(measure, "ListTradeHistory");
+                }
+            }
+            catch (NotConnectedException)
+            { }
+            finally
+            {
+
                 
-				{
-					var measure = Tracker.StartMeasure();
-					ApiClient.LogIn(UserName, Password);
-					Tracker.EndMeasure(measure, "LogIn");
-				}
+                
+                
+                try
+                {
+                    if (ApiClient != null && !String.IsNullOrEmpty(ApiClient.Session))
+                        ApiClient.LogOut();
+                }
+                catch (Exception exc)
+                {
+                    Report(exc);
+                }
 
-				AccountInformationResponseDTO accountInfo;
-				{
-					var measure = Tracker.StartMeasure();
-					accountInfo = ApiClient.AccountInformation.GetClientAndTradingAccount();
-					Tracker.EndMeasure(measure, "GetClientAndTradingAccount");
-				}
+                recorder.Stop();
+                var messages = recorder.GetRequests();
+                recorder.Dispose();
 
-				{
-					var measure = Tracker.StartMeasure();
-					var resp = ApiClient.SpreadMarkets.ListSpreadMarkets("", "",
-						accountInfo.ClientAccountId, 100, false);
-					Tracker.EndMeasure(measure, "ListSpreadMarkets");
-				}
+                string serialized = ApiClient.Serializer.SerializeObject(messages);
+            }
+        }
 
-				{
-					var measure = Tracker.StartMeasure();
-					var resp = ApiClient.News.ListNewsHeadlinesWithSource("dj", "UK", 10);
-					Tracker.EndMeasure(measure, "ListNewsHeadlinesWithSource");
-				}
+        private int Trade(Client client, AccountInformationResponseDTO accountInfo, PriceDTO price,
+            decimal quantity, string direction, IEnumerable<int> closeOrderIds)
+        {
+            var measure = Tracker.StartMeasure();
+            var tradeRequest = new NewTradeOrderRequestDTO
+                {
+                    MarketId = MarketId,
+                    Quantity = quantity,
+                    Direction = direction,
+                    TradingAccountId = accountInfo.SpreadBettingAccount.TradingAccountId,
+                    AuditId = price.AuditId,
+                    BidPrice = price.Bid,
+                    OfferPrice = price.Offer,
+                    Close = closeOrderIds.ToArray(),
+                };
+            var resp = client.TradesAndOrders.Trade(tradeRequest);
+            Tracker.EndMeasure(measure, "Trade");
 
-				{
-					var measure = Tracker.StartMeasure();
-					var resp = ApiClient.Market.GetMarketInformation(MarketId.ToString());
-					Tracker.EndMeasure(measure, "GetMarketInformation");
-				}
+            if (resp.OrderId == 0)
+            {
+                client.MagicNumberResolver.ResolveMagicNumbers(resp);
+                var message = GetResponseDescription(resp);
+                throw new ApplicationException(message);
+            }
 
-				{
-					var measure = Tracker.StartMeasure();
-					var resp = ApiClient.PriceHistory.GetPriceBars(MarketId.ToString(), "MINUTE", 1, "20");
-					Tracker.EndMeasure(measure, "GetPriceBars");
-				}
+            return resp.OrderId;
+        }
 
-				if (AllowTrading)
-				{
-					var price = GetPrice(ApiClient);
+        private static string GetResponseDescription(ApiTradeOrderResponseDTO resp)
+        {
+            var statusText = string.Format("\r\n{0}: {1}\r\n\r\n",
+                resp.Status_Resolved, resp.StatusReason_Resolved);
 
-					var orderId = Trade(ApiClient, accountInfo, price, 1M, "buy", new int[0]);
+            foreach (var apiOrderResponseDto in resp.Orders)
+            {
+                statusText += string.Format("{0}: {1}\r\n",
+                    apiOrderResponseDto.Status_Resolved, apiOrderResponseDto.StatusReason_Resolved);
+            }
 
-					{
-						var measure = Tracker.StartMeasure();
-						ApiClient.TradesAndOrders.ListOpenPositions(accountInfo.SpreadBettingAccount.TradingAccountId);
-						Tracker.EndMeasure(measure, "ListOpenPositions");
-					}
+            return statusText;
+        }
 
-					Trade(ApiClient, accountInfo, price, 1M, "sell", new[] { orderId });
-				}
-				else
-				{
-					var measure = Tracker.StartMeasure();
-					ApiClient.TradesAndOrders.ListOpenPositions(accountInfo.SpreadBettingAccount.TradingAccountId);
-					Tracker.EndMeasure(measure, "ListOpenPositions");
-				}
+        private static PriceDTO GetPrice(Client client)
+        {
+            var streamingClient = client.CreateStreamingClient();
+            var listener = streamingClient.BuildPricesListener(MarketId);
+            var streamingRecorder = new CIAPI.Streaming.StreamingRecorder<PriceDTO>(listener);
+            streamingRecorder.Start();
 
-				{
-					var measure = Tracker.StartMeasure();
-					var tradeHistory = ApiClient.TradesAndOrders.ListTradeHistory(accountInfo.SpreadBettingAccount.TradingAccountId, 20);
-					Tracker.EndMeasure(measure, "ListTradeHistory");
-				}
-			}
-			catch (NotConnectedException)
-			{ }
-			finally
-			{
-				try
-				{
-					if (ApiClient != null && !String.IsNullOrEmpty(ApiClient.Session))
-						ApiClient.LogOut();
-				}
-				catch (Exception exc)
-				{
-					Report(exc);
-				}
-			}
-		}
+            PriceDTO price = null;
+            try
+            {
+                var finished = new ManualResetEvent(false);
 
-		private int Trade(Client client, AccountInformationResponseDTO accountInfo, PriceDTO price,
-			decimal quantity, string direction, IEnumerable<int> closeOrderIds)
-		{
-			var measure = Tracker.StartMeasure();
-			var tradeRequest = new NewTradeOrderRequestDTO
-				{
-					MarketId = MarketId,
-					Quantity = quantity,
-					Direction = direction,
-					TradingAccountId = accountInfo.SpreadBettingAccount.TradingAccountId,
-					AuditId = price.AuditId,
-					BidPrice = price.Bid,
-					OfferPrice = price.Offer,
-					Close = closeOrderIds.ToArray(),
-				};
-			var resp = client.TradesAndOrders.Trade(tradeRequest);
-			Tracker.EndMeasure(measure, "Trade");
+                listener.MessageReceived +=
+                    (s, args) =>
+                    {
+                        price = args.Data;
+                        finished.Set();
+                    };
 
-			if (resp.OrderId == 0)
-			{
-				client.MagicNumberResolver.ResolveMagicNumbers(resp);
-				var message = GetResponseDescription(resp);
-				throw new ApplicationException(message);
-			}
+                if (!finished.WaitOne(TimeSpan.FromMinutes(1)))
+                    throw new ApplicationException("Can't obtain price: timed out");
 
-			return resp.OrderId;
-		}
+                return price;
+            }
+            finally
+            {
 
-		private static string GetResponseDescription(ApiTradeOrderResponseDTO resp)
-		{
-			var statusText = string.Format("\r\n{0}: {1}\r\n\r\n",
-				resp.Status_Resolved, resp.StatusReason_Resolved);
+                streamingRecorder.Stop();
+                var messages = streamingRecorder.GetMessages();
+                var serialized = client.Serializer.SerializeObject(messages);
+                streamingRecorder.Dispose();
 
-			foreach (var apiOrderResponseDto in resp.Orders)
-			{
-				statusText += string.Format("{0}: {1}\r\n",
-					apiOrderResponseDto.Status_Resolved, apiOrderResponseDto.StatusReason_Resolved);
-			}
 
-			return statusText;
-		}
+                listener.Stop();
+                streamingClient.Dispose();
+            }
+        }
 
-		private static PriceDTO GetPrice(Client client)
-		{
-			var streamingClient = client.CreateStreamingClient();
-			var listener = streamingClient.BuildPricesListener(MarketId);
-
-			PriceDTO price = null;
-			try
-			{
-				var finished = new ManualResetEvent(false);
-
-				listener.MessageReceived +=
-					(s, args) =>
-					{
-						price = args.Data;
-						finished.Set();
-					};
-
-				if (!finished.WaitOne(TimeSpan.FromMinutes(1)))
-					throw new ApplicationException("Can't obtain price: timed out");
-
-				return price;
-			}
-			finally
-			{
-				listener.Stop();
-				streamingClient.Dispose();
-			}
-		}
-
-		protected override void Cleanup()
-		{
-			if (ApiClient != null)
-			{
-				ApiClient.Dispose();
-				ApiClient = null;
-			}
-		}
-	}
+        protected override void Cleanup()
+        {
+            if (ApiClient != null)
+            {
+                ApiClient.Dispose();
+                ApiClient = null;
+            }
+        }
+    }
 }
