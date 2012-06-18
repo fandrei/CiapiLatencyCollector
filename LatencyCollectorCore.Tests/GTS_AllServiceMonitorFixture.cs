af9ee959489f51e47dae3ede66b20f4fcb31b0d0
David Laing <david@davidlaing.com>
2012-06-18 18:54:16 +0100

    #83 - add skeleton GTS_AllServiceMonitor

---
+++ LatencyCollectorCore.Tests/GTS_AllServiceMonitorFixture.cs
@@ -0,0 +1,20 @@
+﻿using LatencyCollectorCore.Monitors;
+using NUnit.Framework;
+
+namespace LatencyCollectorCore.Tests
+{
+    [TestFixture]
+    public class GTS_AllServiceMonitorFixture
+    {
+        [Test, Ignore("WIP: Should use playback so this can run without hitting the actual API")]
+        public void ShouldExecuteWithoutExceptions()
+        {
+            var monitor = new GTS_AllServiceMonitor();
+
+            monitor.Start();
+            monitor.Execute();
+
+            monitor.WaitForFinish();
+        }
+    }
+}
