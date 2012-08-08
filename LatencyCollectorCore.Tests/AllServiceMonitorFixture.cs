79768957e4ea9c8e517b8dcaad96704c7860b019
fandrei <andrei_f_11@mail.ru>
2012-08-08 18:28:51 +0700

    added test infrastructure

--- LatencyCollectorCore.Tests/AllServiceMonitorFixture.cs
+++ LatencyCollectorCore.Tests/AllServiceMonitorFixture.cs
@@ -4,28 +4,28 @@ using Salient.ReflectiveLoggingAdapter;
 
 namespace LatencyCollectorCore.Tests
 {
-    [TestFixture]
-    public class AllServiceMonitorFixture
-    {
-        static AllServiceMonitorFixture()
-        {
-            LogManager.CreateInnerLogger =
-                (logName, logLevel, showLevel, showDateTime, showLogName, dateTimeFormat) =>
-                new SimpleTraceAppender(logName, logLevel, showLevel, showDateTime, showLogName, dateTimeFormat);
-        }
+	[TestFixture]
+	public class AllServiceMonitorFixture
+	{
+		static AllServiceMonitorFixture()
+		{
+			LogManager.CreateInnerLogger =
+				(logName, logLevel, showLevel, showDateTime, showLogName, dateTimeFormat) =>
+				new SimpleTraceAppender(logName, logLevel, showLevel, showDateTime, showLogName, dateTimeFormat);
+		}
 
-        [Test, Ignore("WIP: Should use playback so this can run without hitting the actual API")]
-        public void ShouldExecuteWithoutExceptions()
-        {
-            var monitor = new AllServiceMonitor
-                              {
-                                  ServerUrl = "https://ciapipreprod.cityindextest9.co.uk/tradingapi",
-                                  StreamingServerUrl = "https://pushpreprod.cityindextest9.co.uk",
-                                  UserName = "XX019353",
-                                  Password = "password",
-                                  AllowTrading = true
-                              };
-            monitor.Execute();
-        }
-    }
+		[Test]
+		public void ShouldExecuteWithoutExceptions()
+		{
+			var monitor = new AllServiceMonitor
+				{
+					ServerUrl = TestSettings.CiapiTestUrl,
+					StreamingServerUrl = TestSettings.CiapiTestStreamingUrl,
+					UserName = TestSettings.CiapiTestUserName,
+					Password = TestSettings.CiapiTestPassword,
+					AllowTrading = true
+				};
+			monitor.Execute();
+		}
+	}
 }
---
+++ LatencyCollectorCore.Tests/TestSettings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LatencyCollectorCore.Tests
+{
+	static class TestSettings
+	{
+		public static string CiapiTestUserName
+		{
+			get
+			{
+				return Environment.GetEnvironmentVariable("CiapiTestUserName");
+			}
+		}
+
+		public static string CiapiTestPassword
+		{
+			get
+			{
+				return Environment.GetEnvironmentVariable("CiapiTestPassword");
+			}
+		}
+
+		public static string CiapiTestUrl
+		{
+			get
+			{
+				return Environment.GetEnvironmentVariable("CiapiTestUrl");
+			}
+		}
+
+		public static string CiapiTestStreamingUrl
+		{
+			get
+			{
+				return Environment.GetEnvironmentVariable("CiapiTestStreamingUrl");
+			}
+		}
+	}
+}
