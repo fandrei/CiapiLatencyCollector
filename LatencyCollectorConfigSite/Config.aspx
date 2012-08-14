82c6e7668218bb30c0c68ca046794cd30cd96619
fandrei <andrei_f_11@mail.ru>
2012-08-14 15:06:31 +0700

    #106 - added enable/disable polling buttons

---
+++ LatencyCollectorConfigSite/Config.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Config.aspx.cs" Inherits="LatencyCollectorConfigSite.Config" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>AppMetrics Config</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table width="50%">
+            <tr>
+                <td>
+                    <asp:Button ID="EnableButton" runat="server" Text="Enable polling" 
+                        onclick="EnableButton_Click" />
+                </td>
+                <td>
+                    <asp:Button ID="DisableButton" runat="server" Text="Disable polling" 
+                        onclick="DisableButton_Click" />
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ LatencyCollectorConfigSite/Config.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Hosting;
+using AppMetrics.WebUtils;
+
+namespace LatencyCollectorConfigSite
+{
+	public partial class Config : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				WebUtil.CheckIpAddress();
+
+				RefreshButtonsState();
+			}
+			catch (UnauthorizedAccessException)
+			{
+			}
+		}
+
+		private void RefreshButtonsState()
+		{
+			var fileExists = File.Exists(StopFileName);
+			EnableButton.Enabled = fileExists;
+			DisableButton.Enabled = !fileExists;
+		}
+
+		protected void EnableButton_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				WebUtil.CheckIpAddress();
+
+				File.Delete(StopFileName);
+				RefreshButtonsState();
+			}
+			catch (UnauthorizedAccessException)
+			{
+			}
+		}
+
+		protected void DisableButton_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				WebUtil.CheckIpAddress();
+
+				File.WriteAllText(StopFileName, "");
+				RefreshButtonsState();
+			}
+			catch (UnauthorizedAccessException)
+			{
+			}
+		}
+
+		private string StopFileName
+		{
+			get
+			{
+				const string tmp = "~/CIAPILatencyCollectorConfig/stop.txt";
+				return HostingEnvironment.MapPath(tmp);
+			}
+		}
+	}
+}
