0fa7f18a04b1640605dd44ea8460c763257c34ed
fandrei <andrei_f_11@mail.ru>
2012-11-27 17:15:54 +0700

    refactoring

--- LatencyCollectorCore/AppSettings.cs
+++ LatencyCollectorCore/AppSettings.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
-using System.Security.Cryptography;
 using System.Text;
 using System.Xml.Serialization;
 
@@ -14,41 +13,8 @@ using LatencyCollectorCore.Monitors;
 
 namespace LatencyCollectorCore
 {
-	public class AppSettings : AppSettingsBase
+	public class AppSettings : AppMetrics.AgentService.PluginBase.AppSettings
 	{
-		public AppSettings()
-		{
-			SetDefaults();
-		}
-
-		#region Updating settings from the central repository
-
-		public string UserName { get; set; }
-
-		public string PasswordEncrypted { get; set; }
-		static readonly byte[] AdditionalEntropy = { 0x43, 0x71, 0xDE, 0x5B, 0x44, 0x72, 0x45, 0xE3, 0xBE, 0x1E, 0x98, 0x2B, 0xAA };
-
-		[XmlIgnore]
-		public string Password
-		{
-			get
-			{
-				if (PasswordEncrypted.IsNullOrEmpty())
-					return "";
-
-				var encrypted = Convert.FromBase64String(PasswordEncrypted);
-				var data = ProtectedData.Unprotect(encrypted, AdditionalEntropy, DataProtectionScope.LocalMachine);
-				var res = Encoding.UTF8.GetString(data);
-				return res;
-			}
-			set
-			{
-				var data = Encoding.UTF8.GetBytes(value);
-				var encrypted = ProtectedData.Protect(data, AdditionalEntropy, DataProtectionScope.LocalMachine);
-				PasswordEncrypted = Convert.ToBase64String(encrypted);
-			}
-		}
-
 		private string _lastConfigText;
 
 		public bool CheckRemoteSettings()
@@ -115,8 +81,6 @@ namespace LatencyCollectorCore
 			_monitorSettings.PollingDisabled = false;
 		}
 
-		#endregion
-
 		private MonitorSettings _monitorSettings = new MonitorSettings();
 
 		public MonitorSettings MonitorSettings
@@ -124,15 +88,11 @@ namespace LatencyCollectorCore
 			get { return _monitorSettings; }
 		}
 
-		public string UserId { get; set; }
-
-		public string NodeName { get; set; }
-
 		private static AppSettings _instance;
 
 		public static AppSettings Instance
 		{
-			get { return _instance ?? (_instance = Load<AppSettings>()); }
+			get { return _instance ?? (_instance = Load<AppSettings>(FileName)); }
 		}
 
 		public static string Version
@@ -150,43 +110,6 @@ namespace LatencyCollectorCore
 			{
 				UserId = Guid.NewGuid().ToString();
 			}
-
-			UpdateConfigVersion();
 		}
-
-		public void Save()
-		{
-			var directory = Path.GetDirectoryName(FileName);
-			if (!Directory.Exists(directory))
-				Directory.CreateDirectory(directory);
-
-			var s = new XmlSerializer(typeof(AppSettings));
-			using (var writer = new StreamWriter(FileName))
-			{
-				s.Serialize(writer, this);
-			}
-		}
-
-		private void SetDefaults()
-		{
-		}
-
-		#region Conversion from old versions
-
-		public int ConfigVersion { get; set; }
-
-		private const int ActualConfigVersion = 1;
-
-		void UpdateConfigVersion()
-		{
-			if (ConfigVersion == 0)
-			{
-				ConfigVersion = ActualConfigVersion;
-				UserName = "";
-				Password = "";
-			}
-		}
-
-		#endregion
 	}
 }
