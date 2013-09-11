93ddb7e6c40fafb00b492748b5eb9cdeb581f1dc
fandrei <andrei.faber@gmail.com>
2013-09-11 16:49:59 +0700

    additional metrics

--- LatencyCollectorCore/Monitors/CiapiLatencyRecorder.cs
+++ LatencyCollectorCore/Monitors/CiapiLatencyRecorder.cs
@@ -20,13 +20,36 @@ namespace LatencyCollectorCore.Monitors
 		{
 			if (info.Exception != null)
 			{
+				var now = DateTime.UtcNow;
+
 				_tracker.Log(info.Exception);
+
+				if (_lastExceptionTime > DateTime.MinValue)
+				{
+					var timeDiff = now - _lastExceptionTime;
+					_tracker.Log("TimeFromLastException CIAPI", timeDiff.TotalSeconds);
+				}
+
+				_lastExceptionTime = now;
 			}
 			else
 			{
+				var now = DateTime.UtcNow;
+
 				_tracker.LogLatency(info.Uri.ToString(), info.Watch.Elapsed.TotalSeconds);
+
+				if (_lastSuccessTime > DateTime.MinValue)
+				{
+					var timeDiff = now - _lastSuccessTime;
+					_tracker.Log("TimeFromLastSuccess CIAPI", timeDiff.TotalSeconds);
+				}
+
+				_lastSuccessTime = now;
 			}
 		}
+
+		private DateTime _lastSuccessTime = DateTime.MinValue;
+		private DateTime _lastExceptionTime = DateTime.MinValue;
 
 		public override void Start()
 		{
